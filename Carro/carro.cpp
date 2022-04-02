1d99a2f7740cb04b9682487ebbc6494dc2053aca
Jorge Luis Rodríguez González <egroj97@gmail.com>
2022-04-01 20:14:26 -0400

    Clase carro con mutex, setters y getters

---
+++ Carro/carro.cpp
@@ -0,0 +1,81 @@
+#include <carro.hpp>
+
+Carro::Carro(long long id) : id{id}
+{
+}
+
+Carro::~Carro()
+{
+}
+
+void Carro::set_volume(int volume)
+{
+    this->mux.lock();
+    this->volume = volume;
+    this->mux.unlock();
+}
+
+void Carro::set_num_doors(int num_doors)
+{
+    this->mux.lock();
+    this->num_doors = num_doors;
+    this->mux.unlock();
+}
+
+void Carro::set_color(int color)
+{
+    this->mux.lock();
+    this->color = color;
+    this->mux.unlock();
+}
+
+void Carro::set_car_model(int car_model)
+{
+    this->mux.lock();
+    this->car_model = car_model;
+    this->mux.unlock();
+}
+
+void Carro::set_motor_model(int motor_model)
+{
+    this->mux.lock();
+    this->motor_model = motor_model;
+    this->mux.unlock();
+}
+
+void Carro::set_seats_model(int seats_model)
+{
+    this->mux.lock();
+    this->seats_model = seats_model;
+    this->mux.unlock();
+}
+
+int Carro::get_volume()
+{
+    return this->volume;
+}
+
+int Carro::get_num_doors()
+{
+    return this->num_doors;
+}
+
+int Carro::get_color()
+{
+    return this->color;
+}
+
+int Carro::get_car_model()
+{
+    return this->car_model;
+}
+
+int Carro::get_motor_model()
+{
+    return this->motor_model;
+}
+
+int Carro::get_seats_model()
+{
+    return this->seats_model;
+}
--- Carro/carro.hpp
+++ Carro/carro.hpp
@@ -1,3 +1,6 @@
+#ifndef __CARRO_HEADER__
+#define __CARRO_HEADER__
+
 class Carro
 {
 private:
@@ -9,7 +12,26 @@ private:
     int motor_model;
     int seats_model;
     std::mutex mux;
+
 public:
-    Carro(long long id);
+    Carro(long long);
     ~Carro();
+
+    void set_volume(int);
+    void set_num_doors(int);
+    void set_color(int);
+    void set_car_model(int);
+    void set_motor_model(int);
+    void set_seats_model(int);
+
+    int get_volume();
+    int get_num_doors();
+    int get_color();
+    int get_car_model();
+    int get_motor_model();
+    int get_seats_model();
+
+    
 };
+
+#endif __CARRO_HEADER__
