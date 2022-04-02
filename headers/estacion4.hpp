26e6e1b249d9d831331925b78c72a63f3e48f827
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2022-04-02 14:05:35 -0400

    Actualizado atributos y metodo de ejecutar

--- headers/estacion4.hpp
+++ headers/estacion4.hpp
@@ -7,19 +7,20 @@
 
 #include <cadena_traslado.hpp>
 
+const std::string seats_model[2] = { "Tela", "Cuero" };
+
 class Estacion4
 {
 private:
     std::mt19937 generador;
     std::normal_distribution<double> tiempo_procesamiento;
-    std::shared_ptr<Carro> carro;
+    std::shared_ptr<CadenaDeTraslado> cadena_traslado;
 public:
     Estacion4() = default;
-    Estacion4(double seed, double M4, double D4, std::shared_ptr<Carro> c);
+    Estacion4(double seed, double M4, double D4, std::shared_ptr<CadenaDeTraslado> c);
     ~Estacion4() = default;
     
     void ejecutar();
-    
 };
 
 #endif // __ESTACION_4_HEADER__
--- src/estacion4.cpp
+++ src/estacion4.cpp
@@ -14,27 +14,43 @@ Estacion4::Estacion4(
     double seed, 
     double M4, 
     double D4, 
-    std::shared_ptr<Carro> c) : generador(seed), 
-                                tiempo_procesamiento(M4, D4),
-                                carro(c)
+    std::shared_ptr<CadenaDeTraslado> c) :  generador(seed), 
+                                            tiempo_procesamiento(M4, D4),
+                                            cadena_traslado(c)
                                                             
 {
     // empty
 }
 
 /**
- * @brief Proceso que duerme el intervalo de segundo obtenido a través de 
+ * @brief Hilo infinito que duerme el intervalo de segundo obtenido a través de 
  * la distriubción normal (tiempo de procesamiento), se procede a la colocacion
- * de los asientos del carro. 
+ * de los asientos de los carros llegados de la estacion 3. 
  */
 void Estacion4::ejecutar()
 {
-    double intervalo_procesamiento = tiempo_procesamiento(generador);
+    while (true)
+    {
+        auto carro = std::move(cadena_traslado->obtener_carro_actual());
+        
+        if (carro != nullptr)
+        {
+            double intervalo_procesamiento = tiempo_procesamiento(generador);
     
-    std::cerr << "Llego a la Estacion 4 el carro con ID: " << carro->get_id() << '\n';
-    std::cerr << "Tiempo de procesamiento: " << intervalo_procesamiento << '\n';
-    
-    std::this_thread::sleep_for(std::chrono::seconds(int(intervalo_procesamiento) + 1));
-    
-    carro->set_seats_model(generador()%2); // 0 son asientos de tela, 1 son asientos de cuero
+            std::cerr << "Llego a la Estacion 4 el carro con ID: " << carro->get_id() << '\n';
+            std::cerr << "Tiempo de procesamiento Estacion 4 carro con ID " << carro->get_id() << ": " << intervalo_procesamiento << '\n';
+            
+            std::this_thread::sleep_for(std::chrono::seconds(int(intervalo_procesamiento) + 1));
+            
+            carro->set_seats_model(generador()%2); // 0 son asientos de tela, 1 son asientos de cuero
+            
+            std::cerr << "Saliendo de la Estacion 4 el carro con ID:" << carro->get_id() << '\n';
+            std::cerr << "Color=: " << carro->get_color() << '\n';
+            std::cerr << "Modelo= " << carro->get_car_model() << '\n';
+            std::cerr << "Motor=: " << carro->get_motor_model() << '\n';
+            std::cerr << "Asientos= " << seats_model[carro->get_seats_model()]<< '\n';
+            
+            
+        }
+    }
 }
