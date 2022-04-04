ad821506db393e47ac4e7af0bfc313ad2f4611d9
Daniel Galaschow <dgalaschow@modu.one>
2022-04-03 23:33:01 -0400

    start estacion 2

---
+++ headers/estacion2.hpp
@@ -0,0 +1,29 @@
+#ifndef __ESTACION_2_HEADER__
+#define __ESTACION_2_HEADER__ 
+
+#include <chrono>
+#include <random>
+#include <thread>
+
+#include <cadena_traslado.hpp>
+
+const std::string color[11] = { "Negro", "Azul", "Marron", "Gris", 
+    "Verde", "Naranja", "Rosa", "Purpura", "Rojo", "Blanco", "Amarrillo" };
+
+const std::string car_model[2] = { "Coupe", "Sedan" };
+
+class Estacion2
+{
+private:
+    std::mt19937 generador;
+    std::normal_distribution<double> tiempo_procesamiento;
+    std::shared_ptr<CadenaDeTraslado> cadena_traslado;
+public:
+    Estacion2() = default;
+    Estacion2(double seed, double M2, double D2, std::shared_ptr<CadenaDeTraslado> c);
+    ~Estacion2() = default;
+    
+    void ejecutar();
+};
+
+#endif // __ESTACION_2_HEADER__
---
+++ src/estacion2.cpp
@@ -0,0 +1,55 @@
+#include <estacion2.hpp>
+
+#include <iostream>
+
+/**
+ * @brief Construye una nueva Estacion 4
+ *
+ * @param seed Semilla para el generador de numeros aleatorio
+ * @param M2 Media 2
+ * @param D2 Desviación Estandar 2
+ * @param carro Carro recibido desde la estacion 1
+ */
+Estacion2::Estacion2(
+    double seed,
+    double M2,
+    double D2,
+    std::shared_ptr<CadenaDeTraslado> c) : generador(seed),
+                                           tiempo_procesamiento(M2, D2),
+                                           cadena_traslado(c)
+
+{
+    // empty
+}
+
+/**
+ * @brief Hilo infinito que duerme el intervalo de segundo obtenido a través de
+ * la distriubción normal (tiempo de procesamiento), se procede a la colocacion
+ * de los asientos de los carros llegados de la estacion 3.
+ */
+void Estacion2::ejecutar()
+{
+    while (true)
+    {
+        auto carro = std::move(cadena_traslado->obtener_carro_actual());
+        auto volumen = 600;
+
+        if (carro != nullptr)
+        {
+            double intervalo_procesamiento = tiempo_procesamiento(generador);
+
+            std::cerr << "Llego a la Estacion 2 el carro con ID: " << carro->get_id() << '\n';
+            std::cerr << "Tiempo de procesamiento Estacion 2 carro con ID " << carro->get_id() << ": " << intervalo_procesamiento << '\n';
+
+            std::this_thread::sleep_for(std::chrono::seconds(int(intervalo_procesamiento) + 1));
+
+            carro->set_color(generador() % 11); // del 0 a 11 son los posibles colores que puede tener el carro
+
+            carro->set_car_model(volumen > 900? 0 : 1); // 0 es coupe, 1 es sedan
+
+            std::cerr << "Saliendo de la Estacion 2 el carro con ID:" << carro->get_id() << '\n';
+            std::cerr << "Color=: " << carro->get_color() << '\n';
+            std::cerr << "Modelo= " << carro->get_car_model() << '\n';
+        }
+    }
+}
